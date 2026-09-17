68b2627ead0ef3b3db38d517d76cd8c6ca2160d5
Kenzy <01023833096ks@gmail.com>
2026-09-17 17:33:10 +0300

    Replace HashRouter with BrowserRouter for routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { TeamProvider } from "./components/contexts/teamMembersContext.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <TeamProvider>
-      <HashRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </TeamProvider>
   </StrictMode>,
 );
